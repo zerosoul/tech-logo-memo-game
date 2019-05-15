5ec011ff98aca8c9b4e1ad3be2244f81d6651140
tristan <yangguochun@ddweilai.com>
2019-05-15 20:58:16 +0800

    feat: add animates and favicons

--- src/containers/Cards.js
+++ src/containers/Cards.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import Card from '../components/Card';
 import Data from '../data.json';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shuffle } from '../utils';
 
+const FadeInUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 100%, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
 const Wrapper = styled.section`
   display: flex;
   flex-wrap: wrap;
@@ -11,6 +22,8 @@ const Wrapper = styled.section`
   background-color: #fff1;
   padding: 0.8rem 0;
   margin: 0 0.2rem;
+  animation-fill-mode: both;
+  animation: ${FadeInUp} 1s;
 `;
 const LogoTitles = Data.map(logo => {
   return { name: logo.name, title: logo.title };
