2c2ddc719c6625c8e3c509adbd7feabd98d9185d
Tristan <yanggc888@163.com>
2019-05-25 16:20:52 +0800

    Updates

---
+++ static/js/2.e639f4.chunk.js
@@ -0,0 +1,14 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(e,t,i){"use strict";i.r(t);var n=i(0),a=i.n(n),o=i(138),s=i.n(o),c=i(2);const d=Object(c.default)(s.a)`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
+`;t.default=()=>{const{width:e,height:t}=window.screen;return a.a.createElement(d,{width:e,height:t,params:{particles:{number:{value:80,density:{enable:!0,value_area:1500}},line_linked:{enable:!0,opacity:.1},move:{direction:"right",speed:.2},size:{value:2},opacity:{anim:{enable:!0,speed:1,opacity_min:.1}}},interactivity:{events:{onclick:{enable:!0,mode:"push"}},modes:{push:{particles_nb:1}}},retina_detect:!0}})}}}]);
