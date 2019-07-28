6297fbbbfd4e6dd6ab83b131f3c3a2b4a4fb4e88
Tristan <yanggc888@163.com>
2019-07-28 11:59:32 +0800

    fix: core-js error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import App from './App';
 import GlobalStyle from './Global.style';
 
